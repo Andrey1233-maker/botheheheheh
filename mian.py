e386a63b50944c7d7b9544af8ba4809c009d2c38
Andrey1233-maker <65078011+Andrey1233-maker@users.noreply.github.com>
2020-08-08 23:00:21 +0300

    Update mian.py

--- mian.py
+++ mian.py
@@ -1,27 +1,35 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 import os
 
 token = "aa1f21dea247df805c0b3136cd220918dfbc24891a09467de0529b5e62cbc4d2b14353e30144361cd261e"
 
-vk = vk_api.VkApi(token = token)
+vk = vk_api.VkApi(token=token)
 
 api = vk.get_api()
 
 longpoll = VkLongPoll(vk)
 
+
 def send(text, id):
-	vk.method('messages.send',{'user_id' : id, 'message' : text, 'random_id' : 0})
+    vk.method('messages.send', {'user_id': id, 'message': text, 'random_id': 0})
+
 
 for event in longpoll.listen():
-	if event.type == VkEventType.MESSAGE_NEW:
-		if event.to_me:
+    if event.type == VkEventType.MESSAGE_NEW:
+        if event.to_me:
 
-			msg = event.text.lower()
-			id = event.user_id
+            msg = event.text.lower()
+            id = event.user_id
 
-			if msg == "привет":
-				send("LOL", id)
-				
-			else:
-				send("WHAT?", id)
+            if msg == "привет":
+                send("LOL", id)
+
+             if msg == 'приве':
+                keyboard = VkKeyboard(one_time=True)
+                keyboard.add_button('Хочу тян', color=VkKeyboardColor.POSITIVE)
+                keyboard.add_button('Тян не нужны!', color=VkKeyboardColor.NEGATIVE)
+
+            else:
+                send("WHAT?", id)
