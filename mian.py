e8de4438764171ca11e71dbfabe13fd9b964ae1d
Andrey1233-maker <65078011+Andrey1233-maker@users.noreply.github.com>
2020-08-11 10:34:28 +0300

    Update mian.py

--- mian.py
+++ mian.py
@@ -2,7 +2,7 @@ import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 import json
 
-token = "aa1f21dea247df805c0b3136cd220918dfbc24891a09467de0529b5e62cbc4d2b14353e30144361cd261e"
+token = "0364e288c6089c09f525a865a94f98d4111aab1f26bed7c017c06a1271ab3f958fe046b78e90993a01d53"
 
 vk = vk_api.VkApi(token=token)
 
